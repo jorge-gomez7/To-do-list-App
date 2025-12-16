ea7589d1b995c4d8faacdb170b500afb45181cd7
Jorge Gómez <Jorge.agomezdev@gmail.com>
2025-12-15 21:09:21 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'To-do-list-App',
+  webDir: 'www'
+};
+
+export default config;
